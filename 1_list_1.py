73ce92d9d1664f794ddf261e9954462f75d72387
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-03 18:54:54 +0530

    Add files via upload

---
+++ 1_list_1.py
@@ -0,0 +1,18 @@
+# EXAMPLE OF PYTHON LIST AND SOME OPERATIONS [1]
+
+destination = ['Bhavnagar', 'Rajkot', 'Surat', 'Ahmedabad', 'Gandhinagar', 'Jamnagar'] # Creating a list
+
+
+print(destination)  # Printing all values
+
+
+print(destination[0]) # Printing 1 value
+
+
+print(destination[0:5]) #Printing first 5 values
+
+
+print(destination * 2)  #Printing list twice
+
+
+print(destination[1:5]) #Printing 2 to 5 values  
---
+++ 2_list_2.py
@@ -0,0 +1,18 @@
+# EXAMPLE OF PYTHON LIST AND SOME OPERATIONS [2]
+
+items = ['Yagnadeepsinh',21,True,'football',4741,2005,'BCA','python',0,None] # Creating a list
+
+
+print(items)  # Printing all values
+
+
+print(items[0]) # Printing 1 value
+
+
+print(items[0:5]) #Printing first 5 values
+
+
+print(items * 2)  #Printing list twice
+
+
+print(items[1:5]) #Printing 2 to 5 values  
