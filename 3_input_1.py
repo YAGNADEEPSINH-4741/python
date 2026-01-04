d132a80e057c74daad41b766ad7e032ce6107319
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-04 12:04:03 +0530

    Add files via upload

---
+++ 3_input_1.py
@@ -0,0 +1,28 @@
+
+# THE FOLLOWING IS AN EXAMPLE OF TAKING INPUT FROM USER AND DISPLAYING IT(1)
+# NOTE :SINCE NO TYPE CONVERSION ALL THE INPUT TAKEN WILL BE IN STRING FORMAT
+
+
+#TAKING INPUT FROM USER 
+
+name = input("ENTER YOUR NAME :")
+address = input("ENTER YOUR ADDRESS :")
+city = input("ENTER YOUR CITY :")
+pincode = input("ENTER YOUR PINCODE :")
+age = input("ENTER YOUR AGE :")
+weight = input("ENTER YOUR WEIGHT :")
+gender = input("ENTER YOUR GENDER :")
+
+#DISPLAYING OUTPUT
+
+print("\nName is : ",name)
+print("Address is : ",address)
+print("City is : ",city)
+print("Pincode is : ",pincode)
+print("Age is :",age)
+print("Weight is : ",weight)
+print("Gender is : ",gender)
+
+
+
+
---
+++ 4_input_2.py
@@ -0,0 +1,23 @@
+
+
+# THE FOLLOWING IS AN EXAMPLE OF TAKING INPUT FROM USER AND DISPLAYING IT(2)
+# NOTE :SINCE NO TYPE CONVERSION ALL THE INPUT TAKEN WILL BE IN STRING FORMAT
+
+
+#TAKING INPUT FROM USER 
+
+name = input("ENTER YOUR COLLEGE NAME :")
+branch = input("ENTER YOUR BRANCH  :")
+sem = input("ENTER YOUR SEM :")
+hobby = input("ENTER YOUR HOBBY :")
+
+#DISPLAYING OUTPUT
+
+print("\nCollege Name is : ",name)
+print("Branch is : ",branch)
+print("Sem is : ",sem)
+print("Hobby is : ",hobby)
+
+
+
+
