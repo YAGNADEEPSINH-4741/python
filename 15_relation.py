de52a0fb46374ae8943494355fa1b5d55f463a79
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-09 11:57:50 +0530

    Add files via upload

---
+++ 15_relation.py
@@ -0,0 +1,21 @@
+
+num1 = int(input("Enter value for num1")) #Getting first number
+num2 =int(input("Enter value for num2")) #Getting second number
+
+result = num1 == num2  
+print(f"{result} = {num1} == {num2}")
+
+result = num1 != num2  
+print(f"{result} = {num1} != {num2}")
+
+result = num1 < num2  
+print(f"{result} = {num1} < {num2}")
+
+result = num1 > num2 
+print(f"{result} = {num1} > {num2}")
+
+result = num1 <= num2
+print(f"{result} = {num1} <= {num2}")
+
+result = num1 >= num2  
+print(f"{result} = {num1} >= {num2}")
