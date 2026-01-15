d3c81c7deee3b1f88eae48f6032d07648422e5c8
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-15 14:00:46 +0530

    Add files via upload

---
+++ pratice/q7.py
@@ -0,0 +1,6 @@
+#Create a tuple and check whether a specific value is present or not.
+
+tp =(15,10,1,20,18,30,36,14,95)
+s=int(input("Check number u want to search : "))
+check = s in tp
+print(check)
