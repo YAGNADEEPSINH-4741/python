228683c4b2047213d9ecc97eb9a0e2e6c23608cf
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-17 10:18:07 +0530

    Add files via upload

---
+++ pratice/q13.py
@@ -0,0 +1,9 @@
+# 13.Create a dictionary and change/update the value of an existing key.
+
+
+dic ={"name":"abc"}
+dic.update({"name":"xyz"})
+print(dic.get('name'))
+
+dic["name"]="hgk"
+print(dic.get('name'))
