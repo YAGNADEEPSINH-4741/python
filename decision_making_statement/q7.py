046b85dccdee18ac92056ebc5a06d2842347e7ce
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-19 12:04:33 +0530

    Add files via upload

---
+++ decision_making_statement/q7.py
@@ -0,0 +1,9 @@
+# write a program to decide which is cheaper approach to go from ahmedabad to delhi. by car or by train. consider person has his own petrol car  and he prefer to travel by 1st class train 
+
+bycar=(936/8)*96
+bytrain=3410
+
+if bycar<bytrain:
+    print("car is cheaper")
+else:
+    print("train is cheaper")
