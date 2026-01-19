141e9c69a5cfb91238b87af65415b326260e522c
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-19 12:04:14 +0530

    Add files via upload

---
+++ decision_making_statement/q5.py
@@ -0,0 +1,9 @@
+# write a program to find out elder brother from given two brother's age. 
+
+age1=int(input("Enter age of 1 brother :"))
+age2=int(input("Enter age of 2 brother :"))
+
+if age1>age2:
+    print("first brother is elder")
+else:
+    print("second brother is elder")
