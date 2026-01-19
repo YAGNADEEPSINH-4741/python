b49eb37560b18180f7fc85cfddc3f006eae39da0
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-19 12:03:34 +0530

    Add files via upload

---
+++ decision_making_statement/q4.py
@@ -0,0 +1,8 @@
+# write a program to find out whether given year is millennium year or not. using if else decision making statements.
+
+no=int(input("Enter year  :"))
+
+if(no%1000==0):
+    print("millennium")
+else:
+    print("not millennium")
