a459341ccaec41d8464b72cb65a7d961ac031212
YAGNADEEPSINH-4741 <yagnadeepsinh.4741@gmail.com>
2026-01-21 12:20:56 +0530

    Add files via upload

---
+++ decision_making_statement/q13.py
@@ -0,0 +1,31 @@
+"""
+     write a program to findout day of week(monday/tuesday/wednesday) from given date.  
+    https://www.wikihow.com/Calculate-the-Day-of-the-Week
+"""
+
+day=int(input("Enter day of month :"))
+mon=int(input("Enter number of month :"))
+lm=26-0  #Year ending digit
+rf=lm//4 #Divinding by 4
+lm+=rf #Adding with reminder
+lm+=6 #adding 6
+lis=["",0,3,3,6,1,4,6,2,5,0,3,5] #value of table according to calender
+days=["Sunday","Monday","Tuesday","Wednesday","Thrsday","Friday","Sunday"] #days of week
+res=lm+lis[mon]+day #Geting value from lis
+res%=7  #Getting fay index
+print(days[res])
+"""
+
+January	0 (or 6 if leap year)
+February	3 (or 2 if leap year)
+March	3
+April	6
+May	1
+June	4
+July	6
+August	2
+September	5
+October	0
+November	3
+December	5
+"""
