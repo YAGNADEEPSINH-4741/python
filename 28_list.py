44f6f93b5c365d5a762fbca656fee94a9a16204a
Vaghela Yagnadeepsinh <yagnadeepsinh.4741@gmail.com>
2026-01-27 12:27:25 +0530

    Add files via upload

---
+++ 28_list.py
@@ -0,0 +1,8 @@
+# for loop with list 
+# write a program to display only positive value in list 
+list = [12, -45, 7, -3, 89, -22, 0, 34, -9, 56]
+
+for no in list:
+    if no>0:
+        print(no)
+        
